797572b9d29594f01239ef76b0f3441416c42a3b
Jakubko V <050505jakub@gmail.com>
2021-12-12 22:24:45 +0100

    Added Animations

--- game_preparation.cpp
+++ game_preparation.cpp
@@ -5,12 +5,40 @@
 #include "game_preparation.h"
 #include <iostream>
 
+void how_many_players_animation(){
+    for(int i=0;i<20;i++)
+        std::cout << std::endl;
+    std::cout << "    ________" << std::endl;
+    std::cout << "   /________\\   I------------------------------------------------I" << std::endl;
+    std::cout << "   I  O  O  I   I How many players would you like to have (2-6)? I" << std::endl;
+    std::cout << "   I   <>   I   V------------------------------------------------I" <<  std::endl;
+    std::cout << "   \\________/  " << std::endl;
+    std::cout << "   ----II----  " << std::endl;
+    std::cout << "       II     " << std::endl;
+    std::cout << "       /\\      " << std::endl;
+    std::cout << "      /  \\     " << std::endl;
+}
+
+void wrong_number_animation(){
+    for(int i=0;i<20;i++)
+        std::cout << std::endl;
+    std::cout << "    ________" << std::endl;
+    std::cout << "   /________\\   I--------------------------I" << std::endl;
+    std::cout << "   I  O  O  I   I Wrong number! Try again. I" << std::endl;
+    std::cout << "   I   <>   I   V--------------------------I" <<  std::endl;
+    std::cout << "   \\________/  " << std::endl;
+    std::cout << "   ----II----  " << std::endl;
+    std::cout << "       II     " << std::endl;
+    std::cout << "       /\\      " << std::endl;
+    std::cout << "      /  \\     " << std::endl;
+}
+
 unsigned char get_players_amount() {
-    std::cout << "How many players wants to play? (2-6): ";
+    how_many_players_animation();
     std::string players_amount_input;
     while (std::cin >> players_amount_input &&
            (players_amount_input.size() != 1 || 50 > players_amount_input[0] || players_amount_input[0] > 54)) {
-        std::cout << "Wrong number try again: ";
+        wrong_number_animation();
     }
     return players_amount_input[0];
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,65 @@
 #include <iostream>
 #include "timer.h"
 #include "game_preparation.h"
+#include "Windows.h"
+
+void welcome_animation(){
+    for(int i=0;i<20;i++)
+        std::cout << std::endl;
+    std::cout << "    ________" << std::endl;
+    std::cout << "   /________\\   I----------------------------I" << std::endl;
+    std::cout << "   I  O  O  I   I Hello and welcome to...... I" << std::endl;
+    std::cout << "   I   <>   I   V----------------------------I" <<  std::endl;
+    std::cout << "   \\________/  " << std::endl;
+    std::cout << "   ----II----  " << std::endl;
+    std::cout << "       II     " << std::endl;
+    std::cout << "       /\\      " << std::endl;
+    std::cout << "      /  \\     " << std::endl;
+    Sleep(2000);
+    for(int i=0;i<21;i++)
+        std::cout << std::endl;
+    std::cout << "                ******************                " << std::endl;
+    std::cout << "                *WHEEL OF FORTUNE*                " << std::endl;
+    std::cout << "                ******************              " << std::endl;
+    std::cout << "               \\                  /           " << std::endl;
+    std::cout << "                \\    ________    /          " << std::endl;
+    std::cout << "                 \\  /________\\  /            " << std::endl;
+    std::cout << "                  \\ I  O  O  I /             "<< std::endl;
+    std::cout << "                   \\I   <>   I/              "<< std::endl;
+    std::cout << "                    \\________/               " << std::endl;
+    Sleep(2000);
+    for(int i=0;i<20;i++)
+        std::cout << std::endl;
+    std::cout << "    ________" << std::endl;
+    std::cout << "   /________\\   I----------------------------------------------------------I" << std::endl;
+    std::cout << "   I  O  O  I   I My name is FortuneBot and I will be your host for today. I" << std::endl;
+    std::cout << "   I   <>   I   V----------------------------------------------------------I" <<  std::endl;
+    std::cout << "   \\________/  " << std::endl;
+    std::cout << "   ----II----  " << std::endl;
+    std::cout << "       II     " << std::endl;
+    std::cout << "       /\\      " << std::endl;
+    std::cout << "      /  \\     " << std::endl;
+    Sleep(3000);
+
+}
+
+void how_many_rounds_animation(){
+    for(int i=0;i<20;i++)
+        std::cout << std::endl;
+    std::cout << "    ________" << std::endl;
+    std::cout << "   /________\\   I-----------------------------------------------I" << std::endl;
+    std::cout << "   I  O  O  I   I Ok, and how many rounds are we going to play? I" << std::endl;
+    std:: cout << "   I   <>   I   V-----------------------------------------------I" <<  std::endl;
+    std::cout << "   \\________/  " << std::endl;
+    std::cout << "   ----II----  " << std::endl;
+    std::cout << "       II     " << std::endl;
+    std::cout << "       /\\      " << std::endl;
+    std::cout << "      /  \\     " << std::endl;
+}
+
 
 int main() {
+    welcome_animation();
     game_preparation();
     std::system("pause");
     return 0;
