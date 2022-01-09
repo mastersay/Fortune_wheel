334a5703d44c98e49bd4fe3a6ca56b423078f1e8
Jakubko V <050505jakub@gmail.com>
2022-01-09 10:37:00 +0100

    Function answer_basic_question now loops until wrong letter is entered and displays message if you win or enter a wrong letter

--- Answer_questions.cpp
+++ Answer_questions.cpp
@@ -8,24 +8,47 @@
 #include "show_question.h"
 
 void answer_basic_question(std::vector<std::string> question) {
-    char letters[20];
-    char letter_answer;
-    int a = 0;
-    std::cout << std::endl;
-    std::cout << "Your letter: ";
-    std::cin >> letter_answer;
-    std::cout << std::endl;
-    for (char letter: question[1]) {
-        if(letter == letter_answer) {
-            std::cout << letter_answer << " ";
+        int a = 0;
+        int b = 0;
+        while(b == 0) {
+            int c = 0;
+            char letters[20];
+            char letter_answer;
+            std::cout << std::endl;
+            std::cout << "Your letter: ";
+            std::cin >> letter_answer;
+            std::cout << std::endl;
             letters[a] = letter_answer;
+            a++;
+            int nic = 2;
+            for (char letter: question[1]) {
+                int a = 1;
+                if(letter == letter_answer)
+                    c = 1;
+                for(int i = 0;i < 21;i++){
+                    if(letter == letters[i]){
+                        std::cout << letters[i] << " ";
+                        a = 2;
+                    }
+                    else
+                        continue;
+                }
+                if(a == 1){
+                    if (letter != ' ')
+                        std::cout << "_ ";
+                    else{std::cout << "  ";}
+                    nic = 1;
+                }
+            }
+            if(c == 0){
+                std::cout << std::endl;
+                std::cout << "This letter is not it the table!" << std::endl;
+                b = 1;
+            }
+            if (nic == 2){
+                std::cout << std::endl;
+                std::cout << "Congrats! You guessed the word correctly!" << std::endl;
+            }
+
         }
-        else if(letter == letters[a]){
-            std::cout << letters[a] << " ";
-            a = a + 1;
-        }
-        else if (letter != ' ')
-            std::cout << "_ ";
-        else {std::cout << "  ";}
-    }
 }
